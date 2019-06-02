e4bedf2cddf2a9bc19e0afc7f4b1814325bcbdf9
Zhixin Ling <1069066484@qq.com>
2019-06-02 23:24:20 +0800

    init

---
+++ report/KeyAccuracies.py
@@ -0,0 +1,38 @@
+ LeNet5:
+At the end of training, training acc=0.68, public test acc=0.62, private test acc=0.63
+With selected params, training acc=0.67, public test acc=0.63, private test acc=0.64
+
+
+ LeNet5_cv1:
+At the end of training, training acc=0.68, public test acc=0.62, private test acc=0.63
+With selected params, training acc=0.64, public test acc=0.62, private test acc=0.62
+
+
+ Res18:
+At the end of training, training acc=0.99, public test acc=0.7, private test acc=0.73
+With selected params, training acc=0.99, public test acc=0.71, private test acc=0.73
+
+
+ VGG11:
+At the end of training, training acc=0.99, public test acc=0.69, private test acc=0.71
+With selected params, training acc=0.99, public test acc=0.7, private test acc=0.71
+
+
+ VGG13:
+At the end of training, training acc=0.99, public test acc=0.7, private test acc=0.72
+With selected params, training acc=0.98, public test acc=0.71, private test acc=0.72
+
+
+ VGG16:
+At the end of training, training acc=0.99, public test acc=0.7, private test acc=0.72
+With selected params, training acc=0.98, public test acc=0.71, private test acc=0.72
+
+
+ VGG19:
+At the end of training, training acc=0.99, public test acc=0.7, private test acc=0.72
+With selected params, training acc=0.99, public test acc=0.71, private test acc=0.73
+
+
+ VGG19_cv1:
+At the end of training, training acc=0.99, public test acc=0.69, private test acc=0.71
+With selected params, training acc=0.95, public test acc=0.69, private test acc=0.7
